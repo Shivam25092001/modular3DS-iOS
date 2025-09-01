13dc4becc143ea135f7142247d45ae6abff37b6c
Shivam <ss056989@gmail.com>
2025-09-01 13:59:07 +0530

    Initial Commit

---
+++ AuthenticationSdk/AuthenticationSdk.swift
@@ -0,0 +1,9 @@
+//
+//  AuthenticationSdk.swift
+//  AuthenticationSdk
+//
+//  Created by Shivam Nan on 01/09/25.
+//
+
+import Foundation
+
---
+++ AuthenticationSdkTests/AuthenticationSdkTests.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationSdkTests.swift
+//  AuthenticationSdkTests
+//
+//  Created by Shivam Nan on 01/09/25.
+//
+
+import Testing
+@testable import AuthenticationSdk
+
+struct AuthenticationSdkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
